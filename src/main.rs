b541cab869ef0b7e27de57e06a8504c3beb0f362
Dave Filion <dave.filion@gmail.com>
2020-04-15 19:20:04 -0400

    got sample announce response

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::fs;
-use std::io::prelude::*;
-use std::io::Cursor;
-use std::fs::File;
-use dns_lookup::{lookup_host, getnameinfo};
 use rand::Rng;
+use std::io::Cursor;
 
-use lava_torrent::torrent::v1::Torrent;
-use std::net::{UdpSocket, ToSocketAddrs, SocketAddr, IpAddr};
 use bytebuffer::ByteBuffer;
-use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
+use byteorder::{BigEndian, ReadBytesExt};
+use lava_torrent::torrent::v1::Torrent;
+use std::net::{SocketAddr, ToSocketAddrs, UdpSocket};
 
 fn print_byte_array(header: &str, bytes: &[u8]) {
     print!("{} => [", header);
-    for b in bytes {
-        print!(" {:X?}, ", b);
+    for (i, b) in bytes.iter().enumerate() {
+        if i == 0 {
+            print!("{:X?}", b);
+        } else {
+            print!(", {:X?}", b);
+        }
     }
     println!("]");
 }
@@ -22,7 +22,7 @@ fn make_connect_packet() -> Vec<u8> {
     let mut bytebuf = ByteBuffer::new();
 
     // write connection id
-    let conn_id : u64= 0x41727101980; // constant for connecting
+    let conn_id: u64 = 0x41727101980; // constant for connecting
     bytebuf.write_u64(conn_id);
 
     // write action = 0
@@ -43,17 +43,21 @@ fn get_conn_id_from_connect_response(response_buf: &[u8]) -> (u64, Vec<u8>) {
     for i in 8..16 {
         let byte = response_buf[i];
         conn_id_buff[j] = byte;
-        j+=1;
+        j += 1;
     }
     // convert conn id to u64
     let mut byte_rdr = Cursor::new(conn_id_buff);
-    (byte_rdr.read_u64::<BigEndian>().unwrap(), conn_id_buff.to_vec())
+    (
+        byte_rdr.read_u64::<BigEndian>().unwrap(),
+        conn_id_buff.to_vec(),
+    )
 }
 
-fn make_announce_packet(conn_id_bytes: &Vec<u8>,
-                        info_hash_bytes: &Vec<u8>,
+fn make_announce_packet(
+    conn_id_bytes: &Vec<u8>,
+    info_hash_bytes: &Vec<u8>,
     torrent_size: u64,
-    port: u16
+    port: u16,
 ) -> Vec<u8> {
     let mut buf = ByteBuffer::new();
 
@@ -105,7 +109,7 @@ fn make_announce_packet(conn_id_bytes: &Vec<u8>,
     buf.to_bytes()
 }
 
-fn get_socket_addr(announce_url : &str) -> SocketAddr {
+fn get_socket_addr(announce_url: &str) -> SocketAddr {
     let pieces: Vec<String> = announce_url.split("://").map(|s| s.to_string()).collect(); // seperates protocol from url
     let protocol = pieces[0].as_str();
     if protocol != "udp" {
@@ -114,6 +118,46 @@ fn get_socket_addr(announce_url : &str) -> SocketAddr {
 
     let url = pieces[1].as_str();
     url.to_socket_addrs().unwrap().next().unwrap()
+}
+
+fn send_connect_req(sock: &UdpSocket) -> Vec<u8> {
+    let connect_packet = make_connect_packet();
+    print_byte_array("Request", &connect_packet);
+
+    // send message to remote udp port
+    let _result = sock.send(&connect_packet);
+
+    // TODO: retry logic
+    // listen for response
+    let mut response_buf = [0; 16];
+    let _num_bytes = sock.recv(&mut response_buf).expect("Didnt recieve data");
+    print_byte_array("Response", &response_buf);
+
+    // extract conn id
+    let (_conn_id_int, conn_id_bytes) = get_conn_id_from_connect_response(&response_buf);
+    conn_id_bytes
+}
+
+fn send_announce_req(
+    sock: &UdpSocket,
+    conn_id_bytes: &Vec<u8>,
+    info_hash_bytes: &Vec<u8>,
+    torrent_size: u64,
+    port: u16,
+) {
+    // now send announce message and return response
+    let announce_packet =
+        make_announce_packet(&conn_id_bytes, &info_hash_bytes, torrent_size, port);
+    print_byte_array("Announce", &announce_packet);
+
+    let _result = sock.send(&announce_packet);
+
+    // listen for response
+    let mut response_buf = [0; 512];
+    let _num_bytes = sock.recv(&mut response_buf).unwrap();
+    print_byte_array("announce resp", &response_buf);
+
+    // do something with it
 }
 
 fn main() {
@@ -135,43 +179,27 @@ fn main() {
     println!("announce: {}", announce_url);
 
     // // bind socket to local port
-    let local_address  = "0.0.0.0:34254";
+    let local_address = "0.0.0.0:34254";
     let sock = UdpSocket::bind(local_address).expect("Couldnt bind to address");
     println!("udp socket bound to local port: {:?}", sock);
 
-    // // connect to remote addr
+    // connect to remote addr
     let remote_addr = get_socket_addr(announce_url.as_str());
     sock.connect(remote_addr).expect("couldnt connect");
     println!("socket connected to remote addr = {:?}", sock);
 
-    // make connection request packet
-    let connect_packet = make_connect_packet();
-    print_byte_array("Request", &connect_packet);
-
-    // send message to remote udp port
-    sock.send(&connect_packet);
-    // sock.send_to(&connect_packet, url);
-
-    // listen for response
-    let mut response_buf = [0; 16];
-    let _num_bytes = sock.recv(&mut response_buf).expect("Didnt recieve data");
-    print_byte_array("Response", &response_buf);
-
-    // extract conn id
-    let (conn_id_int, conn_id_bytes) = get_conn_id_from_connect_response(&response_buf);
+    // send request packet and return connection id
+    let conn_id_bytes = send_connect_req(&sock);
     print_byte_array("conn id", &conn_id_bytes);
 
-    // now send announce message
-    let announce_packet = make_announce_packet(
+    // send announce request
+    send_announce_req(
+        &sock,
         &conn_id_bytes,
         &info_hash_bytes,
         torrent.length as u64,
-        34254 // default local port
+        34264,
     );
-    print_byte_array("Announce", &announce_packet);
-
-
-
 }
 
 #[cfg(test)]
@@ -182,12 +210,22 @@ mod test {
     fn test_make_announce_packet() {
         let port = 6969;
         let torrent_size = 1; // 1 so its easy to see in byte form
-        let conn_id_bytes = [0x10,  0x38,  0x94,  0xC3,  0x73,  0x6B,  0x76,  0xB2].to_vec();
-        let info_hash_bytes = [0xAA, 0x16, 0x30, 0x38, 0x78, 0x53, 0x79, 0x81, 0x90, 0x75, 0x43, 0x56, 0x11, 0x51, 0x73, 0x33, 0x45, 0x89, 0x19, 0xCC].to_vec();
+        let conn_id_bytes = [0x10, 0x38, 0x94, 0xC3, 0x73, 0x6B, 0x76, 0xB2].to_vec();
+        let info_hash_bytes = [
+            0xAA, 0x16, 0x30, 0x38, 0x78, 0x53, 0x79, 0x81, 0x90, 0x75, 0x43, 0x56, 0x11, 0x51,
+            0x73, 0x33, 0x45, 0x89, 0x19, 0xCC,
+        ]
+        .to_vec();
         let result = make_announce_packet(&conn_id_bytes, &info_hash_bytes, torrent_size, port);
 
         print_byte_array("result", &result);
         assert_eq!(result.len(), 98);
     }
+
+#[test]
+fn test_announce() {
+    let sample_announce_resp = [0x0, 0x0, 0x0, 0x1, 0x65, 0x48, 0x92, 0x2D, 0x0, 0x0, 0x6, 0xB8, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x9, 0x42, 0x6C, 0x62, 0x33, 0x85, 0xD8, 0xDB, 0x5B, 0x8B, 0xEB, 0xE8, 0x74, 0xDB, 0x5B, 0x8B, 0xEB, 0xC8, 0x54, 0xD8, 0x24, 0xF, 0x65, 0xC8, 0xD5, 0xD0, 0x48, 0xC0, 0xE5, 0x83, 0x1E, 0x54, 0x11, 0x35, 0xA9, 0xC8, 0xD5, 0x4F, 0x58, 0xB2, 0x94, 0x60, 0x87, 0x4A, 0x3A, 0x73, 0x24, 0xBF, 0x7F, 0x47, 0x3A, 0xFC, 0x7A, 0x1E, 0xC9, 0x44, 0xA8, 0xB2, 0x15, 0xC8, 0xD5];
+
+}
 
 }
