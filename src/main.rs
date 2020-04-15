c3114046722175391be5871d4ddc06c12e7d693d
Dave Filion <dave.filion@gmail.com>
2020-04-15 17:07:26 -0400

    connect and get id

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::io::prelude::*;
 use std::io::Cursor;
 use std::fs::File;
 use dns_lookup::{lookup_host, getnameinfo};
+use rand::Rng;
 
 use lava_torrent::torrent::v1::Torrent;
 use std::net::{UdpSocket, ToSocketAddrs, SocketAddr, IpAddr};
+use bytebuffer::ByteBuffer;
 
 fn main() {
     // load torrent file data
@@ -45,32 +47,9 @@ fn main() {
     let url = pieces[1].as_str();
     println!("remote tracking url: {}", url);
 
-    // split host and port
-    let url_pieces: Vec<String> = url.split(":").map(|s| s.to_string()).collect();
-    println!("url pieces: {:?}", url_pieces);
-    let host = url_pieces[0].as_str();
-    let port = url_pieces[1].as_str();
-
-    // lookup host ip
-    let ips: Vec<IpAddr> = lookup_host(host).unwrap();
-    println!("remote ips: {:?}", ips);
-    // get the first one
-    let ip = ips.get(0).unwrap().clone();
-    let port= port.parse::<u16>().unwrap();
-    let remote_sock = SocketAddr::new(ip, port);
-
-    let (name, service) = match getnameinfo(&remote_sock, 0) {
-        Ok((n, s)) => (n, s),
-        Err(e) => panic!("failed to lookup sock: {:?}", e),
-    };
-
-    println!("socket name={} service={}", name, service);
-
-
-
-
-    // println!("using remote sock addr: {:?}", remote_sock_addr);
-
+    // convert to remote sock addr
+    let remote_addr = url.to_socket_addrs().unwrap().next().unwrap();
+    println!("remove addr {:?}", remote_addr);
 
     // // bind socket to local port
     let local_address  = "0.0.0.0:34254";
@@ -78,14 +57,63 @@ fn main() {
     println!("udp socket bound to local port: {:?}", sock);
 
     // // connect to remote addr
-    sock.connect(remote_sock).expect("couldnt connect");
-    //
-    // // send message to remote udp port
-    // let msg_buf = "hello".as_bytes();
-    // sock.send_to(msg_buf, url);
-    //
-    // let mut response_buf = [0; 512];
-    // let num_bytes = sock.recv(&mut response_buf).expect("Didnt recieve data");
-    // println!("recv message back {}", num_bytes);
+    sock.connect(remote_addr).expect("couldnt connect");
+    println!("socket connected to remote addr = {:?}", sock);
+
+    // make connection request packet
+    let mut bytebuf = ByteBuffer::new();
+
+    // write connection id
+    let conn_id : u64= 0x41727101980;
+    bytebuf.write_u64(conn_id);
+
+    // write action = 0
+    let action: u32 = 0;
+    bytebuf.write_u32(action);
+
+    // write random tx id (4 bytes)
+    let random_tx_id = rand::thread_rng().gen::<[u8; 4]>();
+    println!("tx-id= {:X?}", random_tx_id);
+    bytebuf.write_bytes(&random_tx_id);
+    println!("buf len: {:?}", bytebuf.len());
+
+    let packet = bytebuf.to_bytes();
+
+    print!("Request => [");
+    for b in &packet {
+        print!(" {:X?}, ", b);
+    }
+    println!("]");
+
+    // send message to remote udp port
+    sock.send_to(&packet, url);
+
+    // listen for response
+    let mut response_buf = [0; 16];
+    let _num_bytes = sock.recv(&mut response_buf).expect("Didnt recieve data");
+
+    print!("Response => [");
+    for i in 0..response_buf.len() {
+        let byte = response_buf[i];
+        print!(" {:X?}, ", byte);
+    }
+    println!("]");
+
+    // get connection id from response (at index 8, 8 bytes)
+    let mut conn_id_buff = [0; 8];
+    let mut j = 0;
+    for i in 8..16 {
+        let byte = response_buf[i];
+        conn_id_buff[j] = byte;
+        j+=1;
+    }
+
+    print!("Conn id => [");
+    for i in 0..conn_id_buff.len() {
+        let byte = conn_id_buff[i];
+        print!(" {:X?}, ", byte);
+    }
+    println!("]");
+
 
 }
